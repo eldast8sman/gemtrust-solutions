f7b4aaba13af8250a17763b242df8ce6735da9be
Oviahon Prosper <oviahonprosperpman32@gmail.com>
2022-11-11 10:02:32 +0100

    First Commit

---
+++ resources/views/terms-conditions.blade.php
@@ -0,0 +1,219 @@
+<!DOCTYPE html>
+<html lang="en">
+<meta http-equiv="content-type" content="text/html;charset=utf-8" />
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    
+    <!-- SEO Meta Tags -->
+    <meta name="description" content="Gemtrust Solutions Ltd referred here as company, holds the right to review the terms and conditions for all members when it deems it fit, as long as it is in the best interest of the business and its members.">
+    <meta name="author" content="Prosper JS">
+
+    <!-- OG Meta Tags to improve the way the post looks when you share the page on LinkedIn, Facebook, Google+ -->
+	<meta property="og:site_name" content="gemtrust" /> <!-- website name -->
+	<meta property="og:site" content="" /> <!-- website link -->
+	<meta property="og:title" content="GEMTRUST - Terms & Conditions"/> <!-- title shown in the actual shared post -->
+	<meta property="og:description" content="Gemtrust Solutions Ltd referred here as company, holds the right to review the terms and conditions for all members when it deems it fit, as long as it is in the best interest of the business and its members." /> <!-- description shown in the actual shared post -->
+	<meta property="og:image" content="" /> <!-- image link, make sure it's jpg -->
+	<meta property="og:url" content="" /> <!-- where do you want your post to link to -->
+	<meta property="og:type" content="article" />
+
+    <!-- Website Title -->
+    <title>GEMTRUST - Terms & Conditions</title>
+    
+    <!-- Styles -->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,400i,700&amp;display=swap&amp;subset=latin-ext" rel="stylesheet">
+    <link href="{{ asset('css/bootstrap.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/fontawesome-all.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/swiper.css') }}" rel="stylesheet">
+	<link href="{{ asset('css/magnific-popup.css') }}" rel="stylesheet">
+	<link href="{{ asset('css/styles.css') }}" rel="stylesheet">
+	
+	<!-- Favicon  -->
+    <link rel="icon" href="images/favicon.png">
+</head>
+<body data-spy="scroll" data-target=".fixed-top">
+    
+    <!-- Preloader -->
+	<div class="spinner-wrapper">
+        <div class="spinner">
+            <div class="bounce1"></div>
+            <div class="bounce2"></div>
+            <div class="bounce3"></div>
+        </div>
+    </div>
+    <!-- end of preloader -->
+    
+
+    <!-- Navigation -->
+    @include("components.header")
+    <!-- end of navigation -->
+
+
+    <!-- Header -->
+    <header id="header" class="ex-header">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <h1>Terms & Conditions</h1>
+                </div> <!-- end of col -->
+            </div> <!-- end of row -->
+        </div> <!-- end of container -->
+    </header> <!-- end of ex-header -->
+    <!-- end of header -->
+
+
+    <!-- Breadcrumbs -->
+    <div class="ex-basic-1">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="breadcrumbs">
+                        <a href="/">Home</a><i class="fa fa-angle-double-right"></i><span>Terms & Conditions</span>
+                    </div> <!-- end of breadcrumbs -->
+                </div> <!-- end of col -->
+            </div> <!-- end of row -->
+        </div> <!-- end of container -->
+    </div> <!-- end of ex-basic-1 -->
+    <!-- end of breadcrumbs -->
+
+
+    <!-- Terms Content -->
+    <div class="ex-basic-2">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">                    
+                    <div class="text-container">
+                        <h3>Terms And Conditions</h3>
+                        <p>Gemtrust Solutions Ltd referred here as company, holds the right to review the terms and conditions for all members when it deems it fit, as long as it is in the best interest of the business and its members.</p>
+                    </div> <!-- end of text-container -->
+
+                    <div class="text-container">
+                        <h4>Membership</h4>
+                        <ul class="list-unstyled li-space-lg indent">
+                            <li class="media">
+                                <i class="fas fa-square"></i>
+                                <div class="media-body">To become a member, each participant must buy a package with a non-refundable fee of the amount specified for that package.</div>
+                            </li>
+                            <li class="media">
+                                <i class="fas fa-square"></i>
+                                <div class="media-body">Every package gives member access to the service offering for that package as long as the network circle is still in progress. (See package description).</div>
+                            </li>
+                            <li class="media">
+                                <i class="fas fa-square"></i>
+                                <div class="media-body">A member can buy multiple packages of one or more categories and benefit from all packages simultaneously. </div>
+                            </li>
+                        </ul>
+                    </div> <!-- end of text-container -->
+
+                    <div class="text-container">
+                        <h4>Termination of Membership</h4>
+                        <ul class="list-unstyled li-space-lg indent">
+                            <li class="media">
+                                <i class="fas fa-square"></i>
+                                <div class="media-body">Packages can be terminated only when the registration is pending or within a 48 hours window after registration. The registration amount will be returned on the company’s next payout day.</div>
+                            </li>
+                            <li class="media">
+                                <i class="fas fa-square"></i>
+                                <div class="media-body">Upon termination of membership, all charges on transaction (by third party service providers) will be borne by the customer.</div>
+                            </li>
+                            <li class="media">
+                                <i class="fas fa-square"></i>
+                                <div class="media-body">Termination must be done in writing from the registered email of the account and addressed to support@gemtrust.com with the subject: TERMINATION.</div>
+                            </li>
+                            <li class="media">
+                                <i class="fas fa-square"></i>
+                                <div class="media-body">All old members who apply for termination of membership for whatever reason(s), will forfeit 50% of pending rewards and all other services rendered will be suspended upon approval of the termination request.</div>
+                            </li>
+                        </ul>
+                    </div> <!-- end of text-container -->
+                    
+                    <div class="text-container">
+                        <h4>Rank Advancements</h4>
+                        <ul class="list-unstyled li-space-lg indent">
+                            <li class="media">
+                                <i class="fas fa-square"></i>
+                                <div class="media-body">Advancement of a member from one rank to another is solely upon completion of the previous rank.</div>
+                            </li>
+                            <li class="media">
+                                <i class="fas fa-square"></i>
+                                <div class="media-body">Benefits attached to a rank is upon completion of the registrations for that rank.</div>
+                            </li>
+                            <li class="media">
+                                <i class="fas fa-square"></i>
+                                <div class="media-body">Members with multiple packages will be addressed with the Largest package they bought and the highest rank attained in that package.</div>
+                            </li>
+                            <li class="media">
+                                <i class="fas fa-square"></i>
+                                <div class="media-body">The highest rank in the system is the “EXECUTIVE”. Upon completion, members can buy another package to run a fresh circle.</div>
+                            </li>
+                        </ul>
+                    </div> <!-- end of text-container -->
+                    
+                    <div class="text-container">
+                        <h4>Benefits, Rewards & Commissions</h4>
+                        <ul class="list-unstyled li-space-lg indent">
+                            <li class="media">
+                                <i class="fas fa-square"></i>
+                                <div class="media-body">All members are entitled to the service offering due the package they have acquired.</div>
+                            </li>
+                            <li class="media">
+                                <i class="fas fa-square"></i>
+                                <div class="media-body">Active members earn a commission on all new members registered directly or indirectly in their structure.</div>
+                            </li>
+                            <li class="media">
+                                <i class="fas fa-square"></i>
+                                <div class="media-body">Payment of commissions is immediate and is redeemable in the next payout day from registrastion.</div>
+                            </li>
+                            <li class="media">
+                                <i class="fas fa-square"></i>
+                                <div class="media-body">Commissions are withdrawals from $20 and above.</div>
+                            </li>
+                            <li class="media">
+                                <i class="fas fa-square"></i>
+                                <div class="media-body">All rewards are paid upon completion of the stage(s) they are tied to.</div>
+                            </li>
+                            <li class="media">
+                                <i class="fas fa-square"></i>
+                                <div class="media-body">The company reserves the right to withhold benefits of a member who acts or have conducted business contrary to company rules.</div>
+                            </li>
+                        </ul>
+                    </div> <!-- end of text-container -->
+                </div>
+            </div> <!-- end of row -->
+        </div> <!-- end of container -->
+    </div> <!-- end of ex-basic -->
+    <!-- end of terms content -->
+
+    
+    <!-- Breadcrumbs -->
+    <div class="ex-basic-1">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="breadcrumbs">
+                        <a href="/">Home</a><i class="fa fa-angle-double-right"></i><span>Terms & Conditions</span>
+                    </div> <!-- end of breadcrumbs -->
+                </div> <!-- end of col -->
+            </div> <!-- end of row -->
+        </div> <!-- end of container -->
+    </div> <!-- end of ex-basic-1 -->
+    <!-- end of breadcrumbs -->
+
+    <!-- Footer area -->
+    @include("components.footer")
+    <!-- End footer area -->
+    
+    	
+    <!-- Scripts -->
+    <script src="{{ asset('js/jquery.min.js') }}"></script> <!-- jQuery for Bootstrap's JavaScript plugins -->
+    <script src="{{ asset('js/popper.min.js') }}"></script> <!-- Popper tooltip library for Bootstrap -->
+    <script src="{{ asset('js/bootstrap.min.js') }}"></script> <!-- Bootstrap framework -->
+    <script src="{{ asset('js/jquery.easing.min.js') }}"></script> <!-- jQuery Easing for smooth scrolling between anchors -->
+    <script src="{{ asset('js/swiper.min.js') }}"></script> <!-- Swiper for image and text sliders -->
+    <script src="{{ asset('js/jquery.magnific-popup.js') }}"></script> <!-- Magnific Popup for lightboxes -->
+    <script src="{{ asset('js/validator.min.js') }}"></script> <!-- Validator.js - Bootstrap plugin that validates forms -->
+    <script src="{{ asset('js/scripts.js') }}"></script> <!-- Custom scripts -->
+</body>
+
+</html>
