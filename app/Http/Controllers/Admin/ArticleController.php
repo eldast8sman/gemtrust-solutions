ca9d88c8d180ea1232d6c1d4a20dd1048126ff86
eldast8sman <omotolaniolurotimi@gmail.com>
2022-11-20 12:51:50 +0100

    Articles in Progress

--- app/Http/Controllers/Admin/ArticleController.php
+++ app/Http/Controllers/Admin/ArticleController.php
@@ -7,6 +7,8 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\StoreArticleRequest;
 use App\Http\Requests\UpdateArticleRequest;
+use App\Models\UploadFile;
+use Illuminate\Support\Collection;
 
 class ArticleController extends Controller
 {
@@ -17,7 +19,81 @@ class ArticleController extends Controller
      */
     public function index()
     {
-        
+        $search = !empty($_GET['search']) ? (string)$_GET['search'] : "";
+        $limit = !empty($_GET['limit']) ? (string)$_GET['limit'] : "";
+        if(!empty($search)){
+            $articles = Article::orderBy('created_at', 'desc')->paginate($limit);
+            if(!empty($articles)){
+                return response([
+                    'status' => 'success',
+                    'message' => 'Articles fetched successfully',
+                    'data' => $articles
+                ], 200);
+            } else {
+                return response([
+                    'status' => 'failed',
+                    'message' => 'No Article was found'
+                ], 404);
+            }
+        } else {
+            $found = [];
+            $search_array = explode(' ', $search);
+            foreach($search_array as $search){
+                if(($search != 'a') && ($search != 'an') && ($search != 'the') && ($search != 'is') && ($search != 'of') && ($search != 'with')
+                && ($search != 'are') && ($search != 'was') && ($search != 'were') && ($search != 'for') && ($search != 'on') && ($search != 'to')
+                && ($search != 'on')){
+                    $articles = Article::where('all_details', 'like', '%'.$search.'%')->get();
+                    if(!empty($articles)){
+                        foreach($articles as $article){
+                            if(isset($found[$article->id])){
+                                $found[$article->id] = $found[$article->id] + 1;
+                            } else {
+                                $found[$article->id] = 1;
+                            }
+                        }
+                    }
+                }            
+            }
+
+            if(!empty($found)){
+                arsort($found);
+                $keys = array_keys($found);
+                $articles = [];
+                foreach($keys as $id){
+                    if(!empty($article = Article::find($id))){
+                        $articles[] = $article;
+                    }
+                }
+
+                if(!empty($articles)){
+                    $articles = Collection::make($articles);
+                    $articles = $articles->paginate($limit);
+                    foreach($articles as $article){
+                        if(!empty($article->filename)){
+                            $article->filename = UploadFile::find($article->filename);
+                        }
+                        if(!empty($article->compressed)){
+                            $article->compressed = UploadFile::find($article->compressed);
+                        }
+                    }
+                    return response([
+                        'status' => 'success',
+                        'message' => 'Articles fetched successfully',
+                        'data' => $articles
+                    ], 200);
+                } else {
+                    return response([
+                        'status' => 'success',
+                        'message' => 'No Article was found for "'.$search.'"'
+                    ], 404);
+                }
+            } else {
+                return response([
+                    'status' => 'success',
+                    'message' => 'No Article was fetched for "'.$search.'"'
+                ], 404);
+            }
+        }
     }
 
     /**
---
+++ app/Http/Controllers/FileController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\UploadFile;
+use Illuminate\Support\Str;
+use Illuminate\Http\Request;
+use Illuminate\Http\UploadedFile;
+use Intervention\Image\Facades\Image;
+use Illuminate\Support\Facades\Storage;
+
+class FileController extends Controller
+{
+    /**
+     * Upload file
+     * 
+     * @param UploadedFile $filepath
+     * 
+     * @return $filepaths
+     */
+    public function uploadFile($filepath, $controller="", $disk='local'){
+        if($filepath instanceof UploadedFile){
+            $time = time();
+            $filename = Str::random().$time;
+            $extension = $filepath->extension();
+            $name = $filename.'.'.$extension;
+            if(($extension == "jpg") || ($extension == "jpeg") || ($extension == "gif") || ($extension == "png")){
+                if($stored = Storage::disk($disk)->putFileAs('img', $filepath, $name)){
+                    $filepaths = [];
+                    $image = UploadFile::create([
+                        'filename' => $filename,
+                        'extension' => $extension,
+                        'disk' => $disk,
+                        'controller' => $controller,
+                        'size' => Storage::disk($disk)->size($stored),
+                        'filepath' => $stored,
+                        'url' => Storage::disk($disk)->url($stored)
+                    ]);
+
+                    $filepaths['image'] = $image->id;
+
+                    $image = Image::make($filepath)->resize(100, null, function($constraint){
+                        $constraint->aspectRatio();
+                        $constraint->upsize();
+                    })->encode($extension, 50);
+
+                    if($compressed = Storage::putFileAs('img/compressed', $filepath, $name)){
+                        $comp_image = UploadFile::create([
+                            'filename' => $filename,
+                            'extension' => $extension,
+                            'disk' => $disk,
+                            'controller' => $controller,
+                            'size' => Storage::disk($disk)->size($compressed),
+                            'filepath' => $compressed,
+                            'url' => Storage::disk($disk)->url($compressed)
+                        ]);
+
+                        $filepaths['compressed'] = $comp_image->id;
+                    }
+
+                    return $filepaths;
+                } else {
+                    return false;
+                }
+            }
+        } else {
+            return false;
+        }
+    }
+}
