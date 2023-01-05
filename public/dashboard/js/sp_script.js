46f3b8b5365fc34d319242db50e61aea38828b08
Oviahon Prosper <oviahonprosperpman32@gmail.com>
2023-01-05 08:59:13 +0100

    local url migrated to live url

--- public/dashboard/js/sp_script.js
+++ public/dashboard/js/sp_script.js
@@ -1,4 +1,4 @@
-var BASE_URL = "http://127.0.0.1:8000/";
+var BASE_URL = "https://develop.gemtrustsolutions.com/";
 var SP_URL = "/signalsProvider/"
 var API_URL = BASE_URL + "api/signal-provider/";
 
--- public/user/assets/js/script.js
+++ public/user/assets/js/script.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://127.0.0.1:8000/";
+var BASE_URL = "https://develop.gemtrustsolutions.com/";
 const USER_URL = "/users/"
 const API_URL = BASE_URL + "api/";
 
